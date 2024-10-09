b45823edce585c3bd3e7a12b05e680825be02336
Pranit <pranit@blocksone.tech>
2024-10-09 20:13:28 +0530

    Added third test case with two numbers to pass the case

--- app.js
+++ app.js
@@ -12,10 +12,20 @@ app.post('/add', (req, res) => {
     //   res.status(200).send({ result: 0 });
 
     //second
+    // const numbers = req.body.numbers;
+    // if (numbers === "") return res.status(200).send({ result: 0 });
+    // if (!numbers.includes(",")) return res.status(200).send({ result: parseInt(numbers) });
+    // res.status(200).send({ result: 0 });
+
+    // case - Third
     const numbers = req.body.numbers;
     if (numbers === "") return res.status(200).send({ result: 0 });
-    if (!numbers.includes(",")) return res.status(200).send({ result: parseInt(numbers) });
-    res.status(200).send({ result: 0 });
+
+    const numArray = numbers.split(',');
+    const result = numArray.reduce((sum, num) => sum + parseInt(num), 0);
+    res.status(200).send({ result });
+   
+      
 });
 
 module.exports = app;
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -9,9 +9,15 @@ describe('POST /add', () => {
 //     expect(res.body.result).to.equal(0);
 //   });
 
-it('returns the number, one number is provided', async () => {
-    const res = await request(app).post('/add').send({ numbers: "1" });
-    expect(res.status).to.equal(200);
-    expect(res.body.result).to.equal(1);
-  });
+    // it('returns the number, one number is provided', async () => {
+    //     const res = await request(app).post('/add').send({ numbers: "1" });
+    //     expect(res.status).to.equal(200);
+    //     expect(res.body.result).to.equal(1);
+    // });
+
+    it('returns the sum of two numbers separated by a comma', async () => {
+        const res = await request(app).post('/add').send({ numbers: "1,5" });
+        expect(res.status).to.equal(200);
+        expect(res.body.result).to.equal(6);
+    });
 });
